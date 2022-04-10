e04a4770df9c97c4564e0b333882857ec4b88b6a
chencheng <chencheng9@kingsoft.com>
2022-04-10 17:07:00 +0800

    init project

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+# 添加 Java 8 镜像来源
+FROM java:8
+VOLUME /tmp
+ARG JAR_FILE
+RUN echo $JAR_FILE
+
+# 添加 Spring Boot 包
+ADD target/${JAR_FILE} /app.jar
+EXPOSE 8080
+# 执行启动命令
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/com/example/hellodocker/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.hellodocker.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chencheng
+ */
+@RestController
+public class IndexController {
+    @RequestMapping(value = "/")
+    public String index() {
+        return "hello docker!";
+    }
+}
